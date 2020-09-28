00fb9ba69eac6991e74b702e3daad6d186f9b1a1
KIL Bismuth <kiralowrey@gmail.com>
2020-09-28 16:05:37 -0700

    Database setup

---
+++ database/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (name)
+VALUES ("McDonalds Dollar Menu Chicken Sandwich");
+
+INSERT INTO burgers (name)
+VALUES ("Krabby Patty");
+
+INSERT INTO burgers (name)
+VALUES ("A cheeseburger. Hold on... Change of plans. A BACON cheeseburger. VVith, VVaffle fries.");
