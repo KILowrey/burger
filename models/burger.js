37e357f45e70b10703ab33745e26ec44d45fc16f
KIL Bismuth <kiralowrey@gmail.com>
2020-09-29 16:17:35 -0700

    drafted const burger

--- models/burger.js
+++ models/burger.js
@@ -3,7 +3,27 @@ const orm = require('../config/orm.js');
 
 // Code that will call the ORM functions using burger specific input for the ORM.
 const burger = {
- // TODO: look at cat.js in 13-MVC, 17-CatsApp, Solved
+ // TODO: make sure this works right. copyied from cat.js on 13-MVC.
+  all: function(callback) {
+    orm.all("burgers", function(res) {
+      callback(res);
+    });
+  },
+  create: function(cols, vals, callback) {
+    orm.create("burgers", cols, vals, function(res) {
+      callback(res);
+    });
+  },
+  update: function(objColVals, condition, callback) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      callback(res);
+    });
+  },
+  delete: function(condition, callback) {
+    orm.delete("burgers", condition, function(res) {
+      callback(res);
+    });
+  }
 };
 
 // Export the database functions for the controller (burgers_controller.js).
