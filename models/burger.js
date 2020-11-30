79047443a96954bc67f8f370ab400ffaa177b726
KIL Bismuth <kiralowrey@gmail.com>
2020-11-30 02:35:24 -0800

    added modle from solution and then converted into promises

--- models/burger.js
+++ models/burger.js
@@ -3,25 +3,38 @@ const orm = require('../config/orm.js');
 
 // Code that will call the ORM functions using burger specific input for the ORM.
 const burger = {
- // TODO: make sure this works right. copyied from cat.js on 13-MVC.
-  all: function(callback) {
-    orm.all("burgers", function(res) {
-      callback(res);
+  // All
+  all: function() {
+    return new Promise (function(resolve,reject) {
+      orm.all('burgers')
+      .then(function(data) {
+        resolve(data);
+      });
     });
   },
-  create: function(cols, vals, callback) {
-    orm.create("burgers", cols, vals, function(res) {
-      callback(res);
+  // Create
+  create: function(name) {
+    return new Promise (function(resolve,reject) {
+      orm.create('burgers', [
+        "burger_name", "devoured"
+      ], [
+        name, false
+      ])
+      .then(function(data) {
+        resolve(data);
+      });
     });
   },
-  update: function(objColVals, condition, callback) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      callback(res);
-    });
-  },
-  delete: function(condition, callback) {
-    orm.delete("burgers", condition, function(res) {
-      callback(res);
+  // Update
+  update: function(id) {
+    return new Promise (function(resolve,reject) {
+      const condition = 'id=' + id;
+      orm.update('burgers', {
+        devoured: true
+      }, condition)
+      .then(function(data) {
+        resolve(data);
+      });
     });
   }
 };
