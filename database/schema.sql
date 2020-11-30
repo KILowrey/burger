489f0b594b71f8370017680bfe13b52063c4935a
KIL Bismuth <kiralowrey@gmail.com>
2020-11-30 02:34:43 -0800

    took inspo from solution for security

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,13 @@
 ### Schema
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers 
-(
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+-- Create the burgers table
+CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
