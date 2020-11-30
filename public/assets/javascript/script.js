157b70b63cd257b2642852aaed13e96496008fdb
KIL Bismuth <kiralowrey@gmail.com>
2020-11-30 02:35:42 -0800

    first draf of frontend javascript

---
+++ public/assets/javascript/script.js
@@ -0,0 +1,37 @@
+// Frontend JavaScript for burger
+// Because I'm using Bulma (pure CSS) instead of Bootstrap (CSS + JS) I have to have more code than others.
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(document).ready(function() {
+  // CREATE a burger
+  $('.create-form').on('submit', function(event) {
+    event.preventDefault();
+    const newBurger = {
+      name: $('#bg').val().trim(),
+      devoured: $('[name=devoured]:false').val().trim()
+    };
+    // Send the POST request.
+    $.agax('/api/burgers', {
+      type: "POST",
+      data: newBurger
+    })
+    .then(function() {
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+  // DEVOUR a burger
+  $('.devour-form').on('submit', function(event) {
+    event.preventDefault();
+    const burger_id = $(this).children('.burger_id').val();
+    // Send the PUT request.
+    $ajax({
+      method: "PUT",
+      url: "/burgers/" + burger_id
+    })
+    .then(function(data) {
+      // Reload the page to display devoured burger in proper column
+      location.reload();
+    });
+  });
+});
