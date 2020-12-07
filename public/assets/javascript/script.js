9ba10a65796138a6e117437f6bd2c26a6b316244
KIL Bismuth <kiralowrey@gmail.com>
2020-12-07 02:23:36 -0800

    delete

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,5 +1,5 @@
 // Frontend JavaScript for burger
-// Because I'm using Bulma (pure CSS) instead of Bootstrap (CSS + JS) I have to have more code than others.
+// Because I'm using Bulma (pure CSS) instead of Bootstrap (CSS + JS) I have to have slightly more code than others.
 
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(document).ready(function() {
@@ -22,7 +22,6 @@ $(document).ready(function() {
   });
   // DEVOUR a burger
   $('.devour-form').on('submit', function(event) {
-    event.preventDefault();
     const burger_id = $(this).children('.burger_id').val();
     // Send the PUT request.
     $ajax({
@@ -31,6 +30,18 @@ $(document).ready(function() {
     })
     .then(function(data) {
       // Reload the page to display devoured burger in proper column
+      location.reload();
+    });
+  });
+  // DELETE a burger
+  $('.delete-form').on("submit", function(event) {
+    const id = $(this).data('id');
+    // Send the DELETE request.
+    $.ajax('/api/burgers/' + id, {
+      type: "DELETE"
+    })
+    .then(function() {
+      // Reload the page to get the updated list
       location.reload();
     });
   });
