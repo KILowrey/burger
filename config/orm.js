5482537a4d90f23dbbaaafe40a6a306f09f8e219
KIL Bismuth <kiralowrey@gmail.com>
2020-12-14 17:52:12 -0800

    debugging

--- config/orm.js
+++ config/orm.js
@@ -4,42 +4,42 @@ const connection = require('./connection.js');
 // Object for all our SQL statement functions.
 const orm = {
   // Find All
-  findAll: function(tableInput) {
-    return new Promise(function(res,rej) {
-      const queryString = "SELECT * FROM " + tableInput + ";";
-      connection.query(queryString, function(err,result) {
-        if(err) throw err;
-        res(result);
+  findAll: function() {
+    return new Promise(function(resolve,reject) {
+      const queryString = "SELECT * FROM burgers;";
+      connection.query(queryString, function(err,data) {
+        if(err) reject(err);
+        resolve(data);
       });
-    });
+    })
   },
   // Create
-  create: function(table, cols, vals) {
-    return new Promise(function(res,rej) {
+  create: function(burger) {
+    return new Promise(function(resolve,reject) {
       const queryString = "INSERT INTO burgers SET ?;";
-      connection.query(queryString, function(err,result) {
-        if (err) throw err;
-        res(result);
+      connection.query(queryString, burger, function(err,data) {
+        if (err) reject(err);
+        resolve(data);
       });
     });
   },
   // Update
-  update: function(table, objColVals, condition) {
-    return new Promise(function(res,rej) {
+  update: function(id, devoured) {
+    return new Promise(function(resolve,reject) {
       const queryString = "UPDATE burgers SET devoured = ? WHERE id = ?;";
-      connection.query(queryString, [devoured, id], function(err,result) {
-        if (err) throw error;
-        res(result);
+      connection.query(queryString, [devoured, id], function(err,data) {
+        if (err) reject(err);
+        resolve(data);
       });
     });
   },
   // Delete
   delete: function(id) {
-    return new Promise(function(res,rej) {
+    return new Promise(function(resolve,reject) {
       const queryString = "DELETE FROM burgers WHERE id = " + id;
-      connection.query(queryString, function(err,result) {
-        if (err) throw err;
-        res(result);
+      connection.query(queryString, function(err,data) {
+        if (err) reject(err);
+        resolve(data);
       });
     });
   }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,28 +5,21 @@ const burger = require('../models/burger.js');
 
 // get route -> index
 router.get('/', function(req,res) {
-  res.redirect('/burgers');
-});
-
-// the main page
-router.get('/burgers', function(req,res) {
-  burger.findAll(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index w/ handlebar
-    res.render('index', { burger_data: burgerData });
-  });
+  burger.findAll().then(function(data){
+    res.render("index", {burgers:data}); // { burger_data: "" }
+  })
 });
 
 // post route -> back to index
-router.post('/burgers/create', function(req,res) {
+router.post('/api/burgers', function(req,res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
-    // render back to index with handle
-    res.redirect('/');
+  burger.create(req.body).then(function(data) {
+    res.json(data);
   });
 });
 
 // put route -> back to index
-router.put('/burgers/:id', function(req,res) {
+router.put('/api/burgers/:id', function(req,res) {
   burger.update(req.params.id, function(result) {
     res.sendStatus(200);
   });
